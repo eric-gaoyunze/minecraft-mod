a590b4c3b0aa6807a0a5fad177735928fcac55a8
eric-gaoyunze <15801434950@163.com>
2024-08-05 19:13:47 +0800

    删掉src，新增e

--- src/main/java/com/example/invoid/item/ModFoods.java
+++
@@ -1,8 +0,0 @@
-package com.example.invoid.item;
-
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.food.Foods;
-
-public class ModFoods {
-    public static final FoodProperties PEANUTE = (new FoodProperties.Builder()).nutrition(1).saturationMod(0.3F).build();
-}
